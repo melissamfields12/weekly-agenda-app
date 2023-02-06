f8e5c48b08b23b5c7b7fdce59f85a0209af84771
Melissa Fields <melissamfields12@gmail.com>
2023-02-06 13:44:25 -0800

    Begin making progress on form.

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,11 +4,35 @@ function Form() {
     const [formType, setFormType] = useState(true)
     const [goalData, setGoalData] = useState({
         name: '',
-        
+        progress: ''
+    })
+    const [taskData, setTaskData] = useState({
+        name: '',
+        time: ''
+    })
+    const [shoppingData, setShoppingData] = useState({
+        name: '',
+        category: ''
     })
 
     return (
-        <div></div>
+        <section id="new-item-form">
+            <h3>Add a new item to your weekly agenda</h3>
+            <form>
+                <select
+                    name="form-type">
+                        <option value="goal">Goal</option>
+                        <option value="task">Task</option>
+                    </select>
+                    <input
+                    className="description"
+                    type="text"
+                    placeholder="Description"
+                    name="name"
+                    value={formType ? goalData.name : taskData.name}
+                    />
+            </form>
+        </section>
     )
 }
 
